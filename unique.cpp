6837dc7e3f6c8494703947d5a6a1869675c85fa9
vsakube <84060884+vsakube@users.noreply.github.com>
2022-11-02 18:02:19 +0100

    Count unique substrings
    Usually this will be of n^2 complexity
    
    But with optimisation we can reduce the complexity
    
    Like I have added optimisation by removing "removing consecutive 1 letter substring and then just counting string length" ....... we can next do the same for 2 letter substring and so on

---
+++ unique.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+#include<unordered_set>
+#include<set>
+#include<string>
+using namespace std;
+
+class Unique{
+
+	unordered_set<string> result ;
+	string str;
+
+	public:	
+	Unique(string _str){ str = _str ; }
+
+	string removeDuplicates(string s)
+	{	
+
+		int n = s.length();
+		string str = "";
+		if (n == 0)
+			return str;
+
+		for (int i = 0; i < n - 1; i++) {
+			if (s[i] != s[i + 1]) {
+				str += s[i];
+			}
+		}	
+		str.push_back(s[n - 1]);
+		return str;
+	}
+
+	bool allCharactersSame()
+	{
+		set <char> s1;
+
+		for ( int i=0 ; i < str.length() ; i++)
+			s1.insert(str[i]);
+	
+		if ( s1.size() == 1 )
+			return true;
+		else
+			return false;;
+	}
+
+	int distinctSubstring()
+	{
+		if(str.length()==0) return 0;
+		if(str.length()==1) return 1;
+
+		if(allCharactersSame()) return str.length();
+
+		for (int i = 0; i <= str.length(); i++)
+		{	
+			for (int j = 2; j <= str.length()-i; j++)
+			{
+
+				result.insert(str.substr(i, j));
+			}	
+		}
+		return result.size() + (removeDuplicates(str)).length();
+	}
+	unordered_set<string>& getset(){
+		return result;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	if(argc !=2 ) return 1;
+	string str(argv[1]);
+	Unique uu(str);
+	cout << uu.distinctSubstring();
+	unordered_set<string> _set = uu.getset();
+	cout << endl;
+	for(auto str:_set) cout << str << " ";
+	cout << endl;
+	return 0;
+
+}
+
+
